e3b4a01526b522956f04d03c3d9a8bd70f2519fb
Brandon Harris <cheeridon@gmail.com>
2017-12-05 15:56:29 -0500

    background pic

--- src/index.js
+++ src/index.js
@@ -2,19 +2,75 @@ document.addEventListener("DOMContentLoaded", function(){
 
   let username = document.getElementById('username').value
   let usernameForm = document.getElementById('submituser')
+  let welcomeContainer = document.getElementById('welcome')
+  let pokemon = document.getElementById('pokemon')
+
 
   function greetUser(username){
-    alert(`Welcome, ${username}`)
+    usernameForm.style.visibility = "hidden";
+    let newUl = document.createElement('ul')
+    newUl.innerText = `Welcome to Fauxkemon, ${username.name}`
+    welcomeContainer.appendChild(newUl)
   }
 
 
   usernameForm.addEventListener("submit", event => {
     event.preventDefault()
     let username = document.getElementById('username').value
-    fetch('http://localhost:3000/users', {method: "post",
-                                        headers: {'Accept': 'application/json',
-                                        'Content-Type': 'application/json'},
-                                        body: JSON.stringify({user:{name: username, pokemon_id: null}})})
-                                        .then(res => res.json()).then(console.log)});
+    fetch("http://localhost:3000/users", {method: "post",
+                                       headers: {'Accept': 'application/json',
+                                       'Content-Type': 'application/json'},
+                                       body: JSON.stringify({user:{name: username, pokemon_id: null}})})
+                                       .then(res => res.json()).then(json => greetUser(json))});
+
+
+
+const canvas = document.querySelector('#leftside')
+const ctx = canvas.getContext("2d");
+
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+let ballRadius = 10;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+    }
+
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 100);
 
 })
