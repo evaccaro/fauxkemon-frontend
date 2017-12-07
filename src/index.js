b76dbd0136d8704929f7329bef40ba97a76e98a4
github username <github email address>
2017-12-06 19:44:55 -0500

    Added initial battle

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function(){
   const TIME_TO_FIRST = 4; // in seconds
   const TIME_TO_SECOND = TIME_TO_FIRST * 2;
   const TIME_TO_THIRD = TIME_TO_SECOND * 2;
+  const DEFENSE_MULTIPLIER = 4;
+  const HP_MULTIPLIER = 1.4;
 
   // Global variables
   let currentTime = 0;
@@ -11,17 +13,18 @@ document.addEventListener("DOMContentLoaded", function(){
   let pokemon;
 
   // Element tags
-  let username = document.getElementById('username').value
-  let usernameForm = document.getElementById('submituser')
-  let welcomeContainer = document.getElementById('welcome')
+  let username = document.getElementById('username').value;
+  let usernameForm = document.getElementById('submituser');
+  let welcomeContainer = document.getElementById('welcome');
+  let pokemonList = document.getElementById('pokemonList');
+  let pokemons = document.getElementById('pokes');
+  let pokeSelector = document.getElementById('pokeSelector');
+  let pokeContainer = document.getElementById('pokeContainer');
+  let pokemonTag = document.getElementById('pokemon');
+  // let battleButton = document.getElementById('battleButton');
 
-  let pokemonList = document.getElementById('pokemonList')
-  let pokemons = document.getElementById('pokes')
-  let pokeSelector = document.getElementById('pokeSelector')
-  let pokeContainer = document.getElementById('pokeContainer')
-  let pokemonTag = document.getElementById('pokemon')
-
-
+  // Event handlers
+  // battleButton.addEventListener("click", battle);
 
   function hidePoke(){
     pokemonList.style.visibility = "hidden";
@@ -32,10 +35,8 @@ document.addEventListener("DOMContentLoaded", function(){
   function findPokemonById(id){
     fetch(`http://localhost:3000/pokemons/${id}`)
     .then(res => res.json())
-    .then(console.log)
+    .then(json => json)
   }
-
-  findPokemonById(1)
 
   function showObject(allPoke){
     allPoke.forEach(poke => {
@@ -69,8 +70,11 @@ document.addEventListener("DOMContentLoaded", function(){
   function showPoke(json, pokemon){
     pokeSelector.style.visibility = "hidden";
     welcomeContainer.style.visibility = "hidden";
+    let newImg = document.createElement('img');
+    newImg.src = "https://www.spriters-resource.com/resources/sheet_icons/4/3701.png"
     let newP = document.createElement('p')
     newP.innerText = `Great choice, ${json.name}! Get ready to train ${pokemon.name}`
+    pokeContainer.appendChild(newImg);
     pokeContainer.appendChild(newP)
   }
 
@@ -123,21 +127,53 @@ document.addEventListener("DOMContentLoaded", function(){
                                        body: JSON.stringify({user:{pokemon_id: pokes}})})
                                        .then(res => res.json()).then(json => {pokemon = json.pokemon
                                          showPoke(json, pokemon)
+                                         battleButton = document.createElement('button');
+                                          battleButton.id = "battleButton"
+                                          battleButton.innerText = "Battle!"
+                                          document.getElementById("battleButtonDiv").appendChild(battleButton);
+                                          battleButton.addEventListener("click", battleClosure(json));
                                         });
   });
 
 
 
-  const leftside = document.querySelector('#leftside')
-  const ctxL = leftside.getContext('2d')
+  // const leftside = document.querySelector('#leftside')
+  // const ctxL = leftside.getContext('2d')
+  //
+  // function draw(){
+  //   ctxL.beginPath();
+  //   ctxL.fillRect(5, 5, 100, 100)
+  //   ctxL.fillStyle = "#0095DD";
+  //   ctxL.closePath();
+  // }
+  // draw()
+function battleClosure(user) {
+   return function (event) {
+    // what's the users pokemon?
 
-  function draw(){
-    ctxL.beginPath();
-    ctxL.fillRect(5, 5, 100, 100)
-    ctxL.fillStyle = "#0095DD";
-    ctxL.closePath();
+    // generate random pokemon to fight
+    fetch(`http://localhost:3000/pokemons/${Math.floor(Math.random() * 10)}`)
+    .then(res => res.json())
+    .then(enemyPokemon => {
+      battle(user.pokemon, enemyPokemon)
+    })
+
+    // show enemy pokemon
+    // show user pokemon
+    // show battle interface
+
+    function battle(userPokemon, enemyPokemon) {
+      
+    }
+    // one turn
+    // ---> choose option
+    // ---> choose do action
+    // ---> check if dead
+    // -----------> if opponent dead, generate win actions
+    // -----------> if you're dead, generate lose actions
+    // -----------> else, do another turn
+
   }
-  draw()
-
+}
 
 });
